f865685d97633aae9f43cf6f9ab10624a0249a20
= <=>
2022-05-06 16:30:19 +0100

    hambuger using react js

---
+++ src/component/Index.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import '../App.css'
+
+function Index() {
+  return (
+<div>
+
+
+
+<input type="checkbox" class="toggler"/>
+
+<div class="hamburger"><div></div>
+</div>
+
+<div class="menu">
+    <div>
+        <ul>
+            <li><a href='#'>Home</a></li>
+            <li><a href='#'>About</a></li>
+            <li><a href='#'>Services</a></li>
+            <li><a href='#'>Contact</a></li>
+        </ul>
+    </div>
+</div>
+
+</div>
+
+
+
+  )
+}
+
+export default Index
